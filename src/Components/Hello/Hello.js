90996b2286589ef817928e9728685274512efd67
michelCodeur <michelpathinvo@gmail.com>
2022-02-08 22:24:33 -0600

    finish css hello page

--- src/Components/Hello/Hello.js
+++ src/Components/Hello/Hello.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import styles from './Hello.module.css';
+import photoMichel from '/Users/promichel/SEI/projects/michel-pathinvo-portfolio/src/Assets/photoMichel.png';
 
 function Hello(props) {
     return (
-			<>
-				<div className={styles.background}>
-					<div className={styles.hello}>
-						<h1>H</h1>
-						<h1>E</h1>
-						<h1>L</h1>
-						<h1>L</h1>
-						<h1>O</h1>
-					</div>
+			<div className={styles.section}>
+				<div className={styles.hello}>
+					<h1>Hi</h1>
+					<h1>T</h1>
+					<h1>H</h1>
+					<h1>E</h1>
+					<h1>R</h1>
+					<h1>E</h1>
 				</div>
-			</>
+				<div className={styles.meet}>
+					<h1>Meet Michel</h1>
+				</div>
+				<div className={styles.nothing}>
+					<h1> Empty</h1>
+				</div>
+				<div className={styles.photo}>
+					<img src={photoMichel} alt="photoMichel"/>
+				</div>
+
+				<div className={styles.developer}>
+					<h1> developer</h1>
+				</div>
+			</div>
 		);
 }
 
 export default Hello;
-
-
