db283a8ab51a1d8abf9c0c25cd013c7bcc429237
michelCodeur <michelpathinvo@gmail.com>
2022-02-15 21:16:51 -0600

    finish projects and start contactme

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import styles from './Resume.module.css';
 
 function Resume(props) {
-    return (
-        <div>
-          <h1> this is resume </h1>  
-        </div>
-    );
+	return (
+		<>
+			<div className={styles.title}>
+				<h1> My Resume </h1>
+			</div>
+			<div>
+				<h2 className={styles.link}>
+					A downloaded version of my resume is available
+					<a href='https://my.visualcv.com/michel-pathinvo/'> here</a>
+				</h2>
+			</div>
+			<h2>Thank you for your consideration!</h2>
+		</>
+	);
 }
 
 export default Resume;
