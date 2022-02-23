d6900ef2b2ce397fd4733e1e3feda61c747c540d
michelCodeur <michelpathinvo@gmail.com>
2022-02-23 15:15:16 -0600

    update footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import styles from './Footer.module.css';
 
-
 function Footer(props) {
-    return (
-			<div className={styles.footerContainer}>
-				<span>MichelPathinvo &copy; | Deployed by Netlify </span>
-			</div>
-		);
+	return (
+		<div className={styles.footerContainer}>
+			<span> &copy; 2022 MichelPathinvo </span>
+		</div>
+	);
 }
 
 export default Footer;
